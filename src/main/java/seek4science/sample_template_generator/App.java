41c4ad6c07189e1ed4e964dfd1dd993209b74067
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-08-30 13:33:42 +0100

    initialised new project

---
+++ src/main/java/seek4science/sample_template_generator/App.java
@@ -0,0 +1,13 @@
+package seek4science.sample_template_generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

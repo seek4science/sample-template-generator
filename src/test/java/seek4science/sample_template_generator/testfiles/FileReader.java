bf4a2a9f489b84d1f9f82841cad12274e581513e
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-08-30 17:29:55 +0100

    removed dependency on apache commons
    it was just used for IOUtils to convert a stream to string. Updated to use Scanner available in pure JDK. Reducing dependencies will make the final ruby gem smaller and easier

--- src/test/java/seek4science/sample_template_generator/testfiles/FileReader.java
+++ src/test/java/seek4science/sample_template_generator/testfiles/FileReader.java
@@ -2,9 +2,8 @@ package seek4science.sample_template_generator.testfiles;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
-import org.apache.commons.io.IOUtils;
 
 public class FileReader {
 
@@ -12,7 +11,9 @@ public class FileReader {
 		return FileReader.class.getResource(name).openStream();
 	}
 
+	@SuppressWarnings("resource")
 	public static String getContents(String name) throws IOException {
-		return IOUtils.toString(getStream(name), StandardCharsets.UTF_8.toString());
+		java.util.Scanner scanner = new Scanner(getStream(name)).useDelimiter("\\A");
+	    return scanner.hasNext() ? scanner.next() : "";
 	}
 }
